15cbb7ead4e5f2d8f86a846627b1b3b73a450f71
raqib_taf <raqib.taf@gmail.com>
2018-09-09 18:06:09 +0600

    fix: change date format

--- src/assets/js/date-range.js
+++ src/assets/js/date-range.js
@@ -17,7 +17,7 @@ $(function() {
     var end = moment();
     var cb = function(start, end) {
       $("#sales-graph .date-range-report span").html(
-        start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY")
+        start.format("ll") + " - " + end.format("ll")
       );
     };
 
@@ -55,7 +55,7 @@ $(function() {
     var end = moment();
     var cb = function(start, end) {
       $("#recent-orders .date-range-report span").html(
-        start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY")
+        start.format("ll") + " - " + end.format("ll")
       );
     };
 
@@ -94,7 +94,7 @@ $(function() {
     var end = moment().subtract(1, "days");
     var cb = function(start, end) {
       $("#user-activity .date-range-report span").html(
-        start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY")
+        start.format("ll") + " - " + end.format("ll")
       );
     };
 
@@ -133,7 +133,7 @@ $(function() {
     var end = moment();
     var cb = function(start, end) {
       $("#analytics-country .date-range-report span").html(
-        start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY")
+        start.format("ll") + " - " + end.format("ll")
       );
     };
 
@@ -172,7 +172,7 @@ $(function() {
     var end = moment();
     var cb = function(start, end) {
       $("#page-views .date-range-report span").html(
-        start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY")
+        start.format("ll") + " - " + end.format("ll")
       );
     };
 
@@ -210,7 +210,7 @@ $(function() {
     var end = moment();
     var cb = function(start, end) {
       $("#activity-user .date-range-report span").html(
-        start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY")
+        start.format("ll") + " - " + end.format("ll")
       );
     };
 
