c96733f5f192a7f6743ac73002a643d5f73b1e26
Abdullah Ar Raqib <raqib.taf@gmail.com>
2018-12-19 19:04:17 +0600

    fix: result box for search

--- src/assets/js/sleek.js
+++ src/assets/js/sleek.js
@@ -230,8 +230,8 @@ SimpleJekyllSearch.init({
   searchInput: document.getElementById('search-input'),
   resultsContainer: document.getElementById('search-results'),
   dataSource: '/assets/data/search.json',
-  searchResultTemplate: '<li><div><a href="{link}">{label}</a></div><div>{location}</div><\/li>',
-  noResultsText: 'No results found',
+  searchResultTemplate: '<li><div class="link"><a href="{link}">{label}</a></div><div class="location">{location}</div><\/li>',
+  noResultsText: '<li>No results found</li>',
   limit: 10,
   fuzzy: true,
 });
