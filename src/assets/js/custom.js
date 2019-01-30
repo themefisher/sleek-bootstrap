7bd93f04dfdd3099986ce41ddc9e9a7d3cfd77f2
Abdullah Ar Raqib <raqib.taf@gmail.com>
2019-01-30 11:24:02 +0600

    fix: jekyll search

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -20,15 +20,18 @@ $(document).ready(function() {
 
   /*======== 1. JEKYLL INSTANT SEARCH ========*/
 
-  SimpleJekyllSearch.init({
-    searchInput: document.getElementById('search-input'),
-    resultsContainer: document.getElementById('search-results'),
-    dataSource: '/assets/data/search.json',
-    searchResultTemplate: '<li><div class="link"><a href="{link}">{label}</a></div><div class="location">{location}</div><\/li>',
-    noResultsText: '<li>No results found</li>',
-    limit: 10,
-    fuzzy: true,
-  });
+  var searchInput = $('#search-input');
+  if(searchInput.length != 0){
+    SimpleJekyllSearch.init({
+      searchInput: document.getElementById('search-input'),
+      resultsContainer: document.getElementById('search-results'),
+      dataSource: '/assets/data/search.json',
+      searchResultTemplate: '<li><div class="link"><a href="{link}">{label}</a></div><div class="location">{location}</div><\/li>',
+      noResultsText: '<li>No results found</li>',
+      limit: 10,
+      fuzzy: true,
+    });
+  }
 
 
   /*======== 2. SCROLLBAR CONTENT ========*/
