85786f399503fb28f434f29b0a84e6b5e0989ab5
faruq.taf <faruq.taf@gmail.com>
2020-03-09 14:23:31 +0600

    fix: todo List

--- js/sleek.js
+++ js/sleek.js
@@ -178,13 +178,12 @@ $(document).ready(function () {
             item.value +
           '</span>' +
           '<div class="task-content">' +
-            '<span class="badge badge-primary">Today</span>' +
             '<span data-dismiss="alert" aria-label="Close">' +
               '<svg class="remove-task" id="Capa_1" enable-background="new 0 0 515.556 515.556" height="16" viewBox="0 0 515.556 515.556" width="16" xmlns="http://www.w3.org/2000/svg"><path d="m64.444 451.111c0 35.526 28.902 64.444 64.444 64.444h257.778c35.542 0 64.444-28.918 64.444-64.444v-322.222h-386.666z"/><path d="m322.222 32.222v-32.222h-128.889v32.222h-161.111v64.444h451.111v-64.444z"/></svg>' +
             '</span>' +
           '</div>' +
         '</div>' +
-        list.innerHTML;
+      list.innerHTML;
       item.value = "";
       //Close input field
       todoInput.classList.toggle("d-block");
