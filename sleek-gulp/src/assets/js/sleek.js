8451c1707d1ea0f49ef60967503570c0b5e00950
faruq.taf <faruq.taf@gmail.com>
2021-03-28 16:44:23 +0600

    add header-home & analytics pages

--- sleek-gulp/src/assets/js/sleek.js
+++ sleek-gulp/src/assets/js/sleek.js
@@ -251,4 +251,37 @@ $(document).ready(function () {
    
   }
 
+  /*======== 10. TOASTER ========*/
+  function callToaster(positionClass) {
+    if (document.getElementById("toaster")) {
+      toastr.options = {
+        closeButton: true,
+        debug: false,
+        newestOnTop: false,
+        progressBar: true,
+        positionClass: positionClass,
+        preventDuplicates: false,
+        onclick: null,
+        showDuration: "300",
+        hideDuration: "1000",
+        timeOut: "5000",
+        extendedTimeOut: "1000",
+        showEasing: "swing",
+        hideEasing: "linear",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut"
+      };
+      toastr.success("Welcome to sleek", "Howdy!");
+    }
+  }
+
+  if (document.dir != "rtl" ){
+    callToaster("toast-top-right");
+  }else {
+    callToaster("toast-top-left");
+  }
+
+  /*======== 11. PROGRESS BAR ========*/
+  NProgress.done();
+
 });
--- sleek-gulp/src/assets/js/vector-map.js
+++ sleek-gulp/src/assets/js/vector-map.js
@@ -18,28 +18,6 @@
 $(document).ready(function() {
   "use strict";
 
-  /*======== 1. JEKYLL INSTANT SEARCH ========*/
-
-  // SimpleJekyllSearch.init({
-  //   searchInput: document.getElementById('search-input'),
-  //   resultsContainer: document.getElementById('search-results'),
-  //   dataSource: '/assets/data/search.json',
-  //   searchResultTemplate: '<li><div class="link"><a href="{link}">{label}</a></div><div class="location">{location}</div><\/li>',
-  //   noResultsText: '<li>No results found</li>',
-  //   limit: 10,
-  //   fuzzy: true,
-  // });
-
-
-
-  /*======== 3. TOOLTIPS AND POPOVER ========*/
-  $('[data-toggle="tooltip"]').tooltip({
-    container: "body",
-    template:
-      '<div class="tooltip" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
-  });
-  $('[data-toggle="popover"]').popover();
-
   /*======== 4. JVECTORMAP DASHBOARD ========*/
   var mapData = {
     US: 1298,
@@ -159,61 +137,4 @@ $(document).ready(function() {
     });
   }
 
-  /*======== 7. MULTIPLE SELECT ========*/
-  // $(".js-example-basic-multiple").select2();
-
-  /*======== 8. LOADING BUTTON ========*/
-  /* 8.1. BIND NORMAL BUTTONS */
-  // Ladda.bind(".ladda-button", {
-  //   timeout: 5000
-  // });
-
-  /* 7.2. BIND PROGRESS BUTTONS AND SIMULATE LOADING PROGRESS */
-  // Ladda.bind(".progress-demo button", {
-  //   callback: function(instance) {
-  //     var progress = 0;
-  //     var interval = setInterval(function() {
-  //       progress = Math.min(progress + Math.random() * 0.1, 1);
-  //       instance.setProgress(progress);
-
-  //       if (progress === 1) {
-  //         instance.stop();
-  //         clearInterval(interval);
-  //       }
-  //     }, 200);
-  //   }
-  // });
-
-  /*======== 9. TOASTER ========*/
-  function callToaster(positionClass) {
-    if (document.getElementById("toaster")) {
-      toastr.options = {
-        closeButton: true,
-        debug: false,
-        newestOnTop: false,
-        progressBar: true,
-        positionClass: positionClass,
-        preventDuplicates: false,
-        onclick: null,
-        showDuration: "300",
-        hideDuration: "1000",
-        timeOut: "5000",
-        extendedTimeOut: "1000",
-        showEasing: "swing",
-        hideEasing: "linear",
-        showMethod: "fadeIn",
-        hideMethod: "fadeOut"
-      };
-      toastr.success("Welcome to sleek", "Howdy!");
-    }
-  }
-
-  if (document.dir != "rtl" ){
-    callToaster("toast-top-right");
-  }else {
-    callToaster("toast-top-left");
-  }
-
-  // /*======== 10. PROGRESS BAR ========*/
-  // NProgress.done();
 });
